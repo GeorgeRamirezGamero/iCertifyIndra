fafd24062cb0618c1539e93fa7d62675bb98c7ee
George Ramirez Gamero <68236558+GeorgeRamirezGamero@users.noreply.github.com>
2020-09-01 15:00:03 +0200

    Update SendMail.java

--- src/main/java/com/indra/icertify/api/util/SendMail.java
+++ src/main/java/com/indra/icertify/api/util/SendMail.java
@@ -25,16 +25,16 @@ public class SendMail {
 		Boolean response = false;
 
 		String to = toEmail;
-		//Inserire mail e password Hotmail, se si usa un altro ricavare lo smtp.host e porta
+		//Inserire mail e password Hotmail, se si usa un altro ricavare lo smtp.host e porta **
 		String from = "";
 		String password = "";
 
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.live.com");
+		Properties props = new Properties();		
+		props.put("mail.smtp.host", "smtp.live.com"); //**
 		props.put("mail.smtp.socketFactory.port", "465");
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "25");
+		props.put("mail.smtp.port", "25"); //**
 		props.put("mail.smtp.starttls.enable", "true");
 
 		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
